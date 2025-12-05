fd4cf11dbcded29e5370651ea8ca993536403855
nikleshdhakad680-sketch <nikleshdhakad680@gmail.com>
2025-12-05 16:06:47 +0530

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,10 @@
+# requirements.py
+required = [
+    "discord.py",
+    "aiohttp",
+    "python-dotenv",
+    "aiosqlite"
+]
+
+print("Run this to install all:")
+print("pip install " + " ".join(required))
